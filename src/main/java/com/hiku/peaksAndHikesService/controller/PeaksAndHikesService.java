1089188b731eeb451207db455e7cc78a9530bf3c
akcabra <andraz.klobucar@gmail.com>
2025-11-09 15:56:24 +0100

    HIKU-13: Added Dockerfile for Peaks and Hikes service

---
+++ src/main/java/com/hiku/peaksAndHikesService/controller/PeaksAndHikesService.java
@@ -0,0 +1,8 @@
+package com.hiku.peaksAndHikesService.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/peaks-hikes")
+public class PeaksAndHikesService extends Application {
+}
