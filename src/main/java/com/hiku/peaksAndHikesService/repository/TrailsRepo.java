fd59911d099b9c8cf023dbce5bfe70f7e933dfb8
Student-na-praksi <filip.gros1@gmail.com>
2025-12-17 23:46:31 +0100

    HIKU-25  cleared peak-hikes-service of useless code, added a Trails.java to retreve all trails from db

--- src/main/java/com/hiku/peaksAndHikesService/repository/TrailsRepo.java
+++ src/main/java/com/hiku/peaksAndHikesService/repository/TrailsRepo.java
@@ -3,20 +3,17 @@ package com.hiku.peaksAndHikesService.repository;
 import com.hiku.shared.geoDataModels.Peak;
 import com.hiku.shared.geoDataModels.Trail;
 
-import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.List;
-
-@ApplicationScoped
 public class TrailsRepo {
 
     private final EntityManagerFactory emf;
 
     public TrailsRepo() {
-        // Create EntityManagerFactory for RESOURCE_LOCAL unit
-        this.emf = Persistence.createEntityManagerFactory("hikuPU");
+        // Create EntityManagerFactory for this service's persistence unit
+        this.emf = Persistence.createEntityManagerFactory("peaksHikesPU");
     }
 
     private EntityManager getEntityManager() {
@@ -27,109 +24,113 @@ public class TrailsRepo {
 
     public List<Trail> findAllTrails() {
         EntityManager em = getEntityManager();
-        List<Trail> result = em.createQuery("SELECT t FROM Trail t", Trail.class).getResultList();
-        em.close();
-        return result;
-    }
-
-    public Trail findTrail(Long id) {
-        EntityManager em = getEntityManager();
-        Trail trail = em.find(Trail.class, id);
-        em.close();
-        return trail;
-    }
-
-    public Trail createTrail(Trail trail) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.persist(trail);
-        em.getTransaction().commit();
-        em.close();
-        return trail;
-    }
-
-    public Trail updateTrail(Trail trail) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        Trail existing = em.find(Trail.class, trail.getId());
-        if (existing == null) {
-            em.getTransaction().rollback();
+        try {
+            // Explicitly query the schema-qualified table to avoid default schema issues
+            return em.createNativeQuery("SELECT * FROM peaks_hikes_service.trails", Trail.class)
+                    .getResultList();
+        } finally {
             em.close();
-            return null;
         }
-        Trail merged = em.merge(trail);
-        em.getTransaction().commit();
-        em.close();
-        return merged;
     }
 
-    public boolean deleteTrail(Long id) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        Trail t = em.find(Trail.class, id);
-        if (t == null) {
-            em.getTransaction().rollback();
-            em.close();
-            return false;
-        }
-        em.remove(t);
-        em.getTransaction().commit();
-        em.close();
-        return true;
-    }
+    // public Trail findTrail(Long id) {
+    //     EntityManager em = getEntityManager();
+    //     Trail trail = em.find(Trail.class, id);
+    //     em.close();
+    //     return trail;
+    // }
+
+    // public Trail createTrail(Trail trail) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     em.persist(trail);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return trail;
+    // }
+
+    // public Trail updateTrail(Trail trail) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     Trail existing = em.find(Trail.class, trail.getId());
+    //     if (existing == null) {
+    //         em.getTransaction().rollback();
+    //         em.close();
+    //         return null;
+    //     }
+    //     Trail merged = em.merge(trail);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return merged;
+    // }
+
+    // public boolean deleteTrail(Long id) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     Trail t = em.find(Trail.class, id);
+    //     if (t == null) {
+    //         em.getTransaction().rollback();
+    //         em.close();
+    //         return false;
+    //     }
+    //     em.remove(t);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return true;
+    // }
 
     // ===== Peak Methods =====
 
-    public List<Peak> findAllPeaks() {
-        EntityManager em = getEntityManager();
-        List<Peak> result = em.createQuery("SELECT p FROM Peak p", Peak.class).getResultList();
-        em.close();
-        return result;
-    }
+    // public List<Peak> findAllPeaks() {
+    //     EntityManager em = getEntityManager();
+    //     List<Peak> result = em.createQuery("SELECT p FROM Peak p", Peak.class).getResultList();
+    //     em.close();
+    //     return result;
+    // }
 
-    public Peak findPeak(Long id) {
-        EntityManager em = getEntityManager();
-        Peak peak = em.find(Peak.class, id);
-        em.close();
-        return peak;
-    }
+    // public Peak findPeak(Long id) {
+    //     EntityManager em = getEntityManager();
+    //     Peak peak = em.find(Peak.class, id);
+    //     em.close();
+    //     return peak;
+    // }
 
-    public Peak createPeak(Peak peak) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        em.persist(peak);
-        em.getTransaction().commit();
-        em.close();
-        return peak;
-    }
+    // public Peak createPeak(Peak peak) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     em.persist(peak);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return peak;
+    // }
 
-    public Peak updatePeak(Peak peak) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        Peak existing = em.find(Peak.class, peak.getId());
-        if (existing == null) {
-            em.getTransaction().rollback();
-            em.close();
-            return null;
-        }
-        Peak merged = em.merge(peak);
-        em.getTransaction().commit();
-        em.close();
-        return merged;
-    }
+    // public Peak updatePeak(Peak peak) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     Peak existing = em.find(Peak.class, peak.getId());
+    //     if (existing == null) {
+    //         em.getTransaction().rollback();
+    //         em.close();
+    //         return null;
+    //     }
+    //     Peak merged = em.merge(peak);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return merged;
+    // }
 
-    public boolean deletePeak(Long id) {
-        EntityManager em = getEntityManager();
-        em.getTransaction().begin();
-        Peak p = em.find(Peak.class, id);
-        if (p == null) {
-            em.getTransaction().rollback();
-            em.close();
-            return false;
-        }
-        em.remove(p);
-        em.getTransaction().commit();
-        em.close();
-        return true;
-    }
+    // public boolean deletePeak(Long id) {
+    //     EntityManager em = getEntityManager();
+    //     em.getTransaction().begin();
+    //     Peak p = em.find(Peak.class, id);
+    //     if (p == null) {
+    //         em.getTransaction().rollback();
+    //         em.close();
+    //         return false;
+    //     }
+    //     em.remove(p);
+    //     em.getTransaction().commit();
+    //     em.close();
+    //     return true;
+    // }
 }
