392e627d91041565bb5d81890e12131e36ec185a
akcabra <andraz.klobucar@gmail.com>
2026-01-03 20:41:07 +0100

    HIKU-31: Spremembe za CICD in dodan Keycloak

--- src/main/java/com/hiku/peaksAndHikesService/PeaksAndHikesService.java
+++ src/main/java/com/hiku/peaksAndHikesService/PeaksAndHikesService.java
@@ -2,7 +2,11 @@ package com.hiku.peaksAndHikesService;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.auth.LoginConfig;
+import javax.annotation.security.DeclareRoles;
 
-@ApplicationPath("/peaks-hikes")
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
+@ApplicationPath("/api/peaks-hikes")
 public class PeaksAndHikesService extends Application {
 }
